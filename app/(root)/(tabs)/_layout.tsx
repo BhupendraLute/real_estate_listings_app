0bdc400e9720f91c5c316ea3fafb13c7e59e2358
Bhupendra Lute <bhupendralute1234@gmail.com.com>
2026-06-16 15:13:05 +0530

    feat: integrate MaterialIcons for tab icons and enable Clerk plugin

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,25 +1,28 @@
-import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { Icon, Label, NativeTabs, VectorIcon } from "expo-router/unstable-native-tabs";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function TabLayout() {
 	return (
 		<NativeTabs>
 			<NativeTabs.Trigger name="index">
 				<Label>Home</Label>
-				<Icon sf="house.fill" />
+				<Icon src={<VectorIcon family={MaterialIcons} name="home" />} />
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="search">
-				<Icon sf="magnifyingglass" />
+				<Icon src={<VectorIcon family={MaterialIcons} name="search" />} />
 				<Label>Search</Label>
 			</NativeTabs.Trigger>
 
 			{/* create property button */}
 
 			<NativeTabs.Trigger name="saved">
-				<Icon sf="heart.fill" />
+				<Icon src={<VectorIcon family={MaterialIcons} name="favorite" />} />
 				<Label>Saved</Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="profile">
-				<Icon sf="person.fill" />
+				<Icon
+					src={<VectorIcon family={MaterialIcons} name="account-circle" />}
+				/>
 				<Label>Profile</Label>
 			</NativeTabs.Trigger>
 		</NativeTabs>
